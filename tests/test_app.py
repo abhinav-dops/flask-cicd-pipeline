28e38848869d312ec2d6f042b47d3da168b6ff68
abhinav-dops <abhinavsingh.ag@gmail.com>
2025-11-07 19:09:48 +0530

    Simplified CI/CD workflow (removed tests)

--- tests/test_app.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-def test_home_route():
-    app = create_app()
-    client = app.test_client()
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b"Hello from CI/CD Pipeline!" in response.data
